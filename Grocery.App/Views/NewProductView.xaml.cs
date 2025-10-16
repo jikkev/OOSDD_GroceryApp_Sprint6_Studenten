3f7a28aaccca574ee7f10fba3afd4198993235b0
Jikkev <s1198343@student.windesheim.nl>
2025-10-16 14:37:27 +0200

    Maak een NewProductView voor het invoerscherm.

---
+++ Grocery.App/Views/NewProductView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Grocery.App.ViewModels;
+
+namespace Grocery.App.Views;
+
+public partial class NewProductView : ContentPage
+{
+    public NewProductView(NewProductViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is NewProductViewModel viewModel)
+        {
+            viewModel.OnAppearing();
+        }
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        if (BindingContext is NewProductViewModel viewModel)
+        {
+            viewModel.OnDisappearing();
+        }
+    }
+}
